b00abf8a5f16fb484bbf468cf942418387be5792
Flydecisive <flydecisive@yandex.ru>
2022-03-01 17:35:19 +0300

    Stepik

---
+++ Stepik2.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Stepik2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String text = scan.nextLine();
+        scan.close();
+        System.out.println(text);
+        System.out.println(text);
+        System.out.print(text);
+    }
+}
