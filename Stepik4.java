9bc52017250985f8c9dfb200d74e52c1d5cfa412
Flydecisive <flydecisive@yandex.ru>
2022-03-01 17:44:03 +0300

    Stepik

---
+++ Stepik4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Stepik4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int temperature = scan.nextInt();
+        System.out.print("Температура воздуха сегодня: " + temperature + " градусов.");
+    }
+}
